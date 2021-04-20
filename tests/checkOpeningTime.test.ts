79a117d9ddc186f4d21649ab21ab5e3c78285400
farid boussetta <farid_2312@hotmail.fr>
2021-04-20 21:19:30 +0200

    add open at and open until fuction

--- tests/checkOpeningTime.test.ts
+++ tests/checkOpeningTime.test.ts
@@ -9,7 +9,7 @@ test('classic cases', () => {
   // 2016-10-01 => saturday
   expect(
     new OH(
-      'Mo 11:00-13:00,18:00-22:00; Th 11:00-13:00,18:00-22:00; Fr 11:00-13:00,18:00-22:00; Sa 11:00-13:00,18:00-22:00',
-    ).checkOpeningTime(new Date('2021-04-20 15:00')),
-  ).toMatchObject({ open: false, openAt: '11:00', openUntil: null, weekDay: 'thirsday' });
+      'Mo 11:00-13:00,18:00-22:00; Tu 11:00-13:00,18:00-22:00; Fr 11:00-13:00,18:00-22:00; Sa 11:00-13:00,18:00-22:00',
+    ).checkOpeningTime(new Date('2021-04-20 11:00')),
+  ).toMatchObject({ open: true, openAt: null, openUntil: '13:00', weekDay: null });
 });
